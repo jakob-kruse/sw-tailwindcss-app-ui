612b69fece5ef4dfd0ca19bf517005758da79154
Jakob Kruse <jakob.kruse@tuta.io>
2020-11-12 17:38:18 +0100

    Added Assets

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function ({ addComponents }) {
-  addComponents({
-    ".sw-button": require("./components/sw-button")(),
-    ".sw-field": require("./components/sw-field")(),
-    ".sw-block-field": require("./components/sw-block-field")(),
-  });
-};
